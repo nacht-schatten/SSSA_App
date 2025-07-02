185dd751618b7c075c61217f4b712104eea34b6c
nacht-schatten <joh3011@hotmail.de>
2025-07-02 17:04:30 +0200

    Update SSSA.py

--- SSSA.py
+++ SSSA.py
@@ -305,8 +305,8 @@ with tab2:
         st.subheader("🔎 Geheimnis rekonstruieren")
 
         punkte_input = st.text_input("📥 Wertepunkte (x,y;x,y;...)", value="1,45;2,63;3,28")
-        p = st.number_input("Primzahl (p)", min_value=2, value=89)
-        k = st.number_input("Benötigte Anteile (k)", min_value=2, value=3)
+        p = st.number_input("Primzahl (p)", min_value=1, value=89)
+        k = st.number_input("Benötigte Anteile (k)", min_value=1, value=3)
 
         try:
             punkte = [tuple(map(int, pair.split(","))) for pair in punkte_input.split(";") if pair.strip()]
